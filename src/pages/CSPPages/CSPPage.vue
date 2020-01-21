c42292ea81cb582191b918f3eafc3da0dfbd929b
William Simiao <williamsimiao@gmail.com>
2020-01-21 18:06:23 -0300

    added pages files

---
+++ src/pages/CSPPages/CSPPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-card
+      class="mx-auto"
+      max-width="344"
+      outlined
+    >
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="overline mb-4">OVERLINE</div>
+          <v-list-item-title class="headline mb-1">{{ pageName + ' Page' }}</v-list-item-title>
+          <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+        </v-list-item-content>
+
+      </v-list-item>
+
+      <v-card-actions>
+        <v-btn text>Button</v-btn>
+        <v-btn text>Button</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      pageName: 'CSP'
+    }
+  }
+}
+</script>
